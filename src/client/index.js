f4c90b854751759e4fe3ee077b09c4d7b990f688
Hanqiu Jiang <hanq.jiang@gmail.com>
2020-04-10 16:20:44 +0200

    feat: Import all .scss files in one scss file

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,6 @@
 import { checkInputType } from './js/inputTypeChecker'
 import { handleSubmit } from './js/formHandler'
-import './styles/base.scss';
-import './styles/footer.scss';
-import './styles/form.scss';
-import './styles/header.scss';
+import './styles/style.scss';
 
 export {
   checkInputType,
